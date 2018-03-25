51a06be3ec6b0bd377bbc3fa936cb88991fa03ca
Дмитрий <glotovdi@gmail.com>
2018-03-25 14:31:03 +0300

    DZ1 / add readme + any modules

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
-# AngularWeatherApp
-
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.7.3.
-
 ## О проекте
 
 Проект в рамках курса Codedojo - Angular in Action
+
+## Модульность
+
+* core
+  * material
+  * user-features
+* main
+  * favourites
+  * recommendations
+  * weakly-weather
+* not-found
+* shared
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FavouritesModule } from './favourites/favourites.module';
+import { WeeklyWeatherModule } from './weekly-weather/weekly-weather.module';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule],
   declarations: []
 })
-export class MainModule { }
+export class MainModule {}
