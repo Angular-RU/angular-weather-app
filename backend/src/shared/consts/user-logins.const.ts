484bac9e563137541ef03916f5fd075fad4d9779
Дмитрий <glotovdi@gmail.com>
2018-04-08 12:16:23 +0300

    fix folder bug

--- backend/src/shared/consts/user-logins.const.ts
+++ backend/src/shared/consts/user-logins.const.ts
@@ -1,5 +1,5 @@
 export const userLoginConst = [
   { login: 'admin', password: 'root' },
   { login: 'codedojo', password: 'codedojo' },
-  { login: 'angular_ru', password: 'angular_ru' },
+  { login: 'angular_ru', password: 'angular_ru' }
 ];
